4716cc8114dc851a3aace22593acaa2eeb995c96
Brad Kerr <134312764+Bradpk@users.noreply.github.com>
2023-06-21 15:54:45 -0400

    g

--- js/main.js
+++ js/main.js
@@ -1,9 +1,47 @@
+// Testing JavaScript
 console.log("Test")
 
+// Stores player turn, tile content and game condition as object properties
 let gamestate = {
     playerTurn: 'X',
     tileContent: ' ',
     gameCondition: 'incomplete'
 }
 
+// Create and render all the HTML elements needed
+function renderElements () {
 
+}
+
+// Sets the initial state of the game
+function startGame () {
+
+}
+
+// Controls what happens when you click on the tiles
+function clickAction () {
+
+}
+
+// Switches the player symbol. 
+function switchPlayer () {
+
+}
+
+// Checks whether game is a win, draw or incomplete
+function checkCondition () {
+
+}
+
+
+
+// Wait, all this is going to do is call the startGame function. What is even the point of this function? 
+function resetGame () {
+
+}
+
+
+
+
+// Could make all the functions stand alone which result in having to call the functions more within other functions?
+// Or I could combine them into larger functions?
