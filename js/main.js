7611ade44bf2b662b55120cb4aabb706e74adb59
Brad Kerr <134312764+Bradpk@users.noreply.github.com>
2023-06-23 08:54:09 -0400

    g

--- js/main.js
+++ js/main.js
@@ -10,47 +10,47 @@ let gameState = {
 //----------------------------------------------------------------------------------------------------------------------------------
 // Creates and renders all the HTML elements needed
 function renderElements() {
-const app = document.getElementById('app')
-app.innerHTML = ''
+    const app = document.getElementById('app')
+    app.innerHTML = ''
 
-// Creates the Title
-const title = document.createElement('h1')
-title.textContent = 'Tic Tac Toe'
-app.appendChild(title)
+    // Creates the Title
+    const title = document.createElement('h1')
+    title.textContent = 'Tic Tac Toe'
+    app.appendChild(title)
 
-// Creates the paragraph that indicates whos turn it is
-const playerTurnParagraph = document.createElement('p')
-playerTurnParagraph.textContent = `Player ${gameState.playerTurn} 's Turn`
-app.appendChild(playerTurnParagraph)
+    // Creates the paragraph that indicates whos turn it is
+    const playerTurnParagraph = document.createElement('p')
+    playerTurnParagraph.textContent = `Player ${gameState.playerTurn} 's Turn`
+    app.appendChild(playerTurnParagraph)
 
-// Creates a div with a class of 'row' 
-const tileContainer = document.createElement('div')
-tileContainer.classList.add('row')
-app.appendChild(tileContainer)
+    // Creates a div with a class of 'row' 
+    const tileContainer = document.createElement('div')
+    tileContainer.classList.add('row')
+    app.appendChild(tileContainer)
 
-// Creates the 9 tiles that make up the gameboard
-for (let i=0; i < 9; i++) {
-    const tile = document.createElement('div')
-    tile.className = 'col-4'
-    tile.textContent = gameState.gameBoard[i]
-    tile.addEventListener('click', () => clickAction(i)) 
-    tileContainer.appendChild(tile)
-}
+    // Creates the 9 tiles that make up the gameboard
+    for (let i = 0; i < 9; i++) {
+        const tile = document.createElement('div')
+        tile.className = 'col-4'
+        tile.textContent = gameState.gameBoard[i]
+        tile.addEventListener('click', () => clickAction(i))
+        tileContainer.appendChild(tile)
+    }
 
-// Creates the paragraph that indicates the game result
-const gameResultParagraph = document.createElement('p')
-if (gameState.gameCondition === 'win') { //<---Check this section later. win and draw not defined yet
-    gameResultParagraph.textContent = `Player ${gameState.playerTurn} wins!`
-} else if (gameState.gameCondition === 'draw') {
-    gameResultParagraph.textContent = 'It\'s a draw!'
-}
-app.appendChild(gameResultParagraph)
+    // Creates the paragraph that indicates the game result
+    const gameResultParagraph = document.createElement('p')
+    if (gameState.gameCondition === 'win') { //<---Check this section later. win and draw not defined yet
+        gameResultParagraph.textContent = `Player ${gameState.playerTurn} wins!`
+    } else if (gameState.gameCondition === 'draw') {
+        gameResultParagraph.textContent = 'It\'s a draw!'
+    }
+    app.appendChild(gameResultParagraph)
 
-// Creates the Reset Button
-const resetButton = document.createElement('button')
-resetButton.textContent = 'Reset Game'
-resetButton.addEventListener('click', initGame) 
-app.appendChild(resetButton)
+    // Creates the Reset Button
+    const resetButton = document.createElement('button')
+    resetButton.textContent = 'Reset Game'
+    resetButton.addEventListener('click', initGame)
+    app.appendChild(resetButton)
 }
 //----------------------------------------------------------------------------------------------------------------------------------
 // Sets the initial state of the game 
@@ -63,33 +63,33 @@ function initGame() {
 //----------------------------------------------------------------------------------------------------------------------------------
 // Controls what happens when you click on the tiles
 function clickAction(tileIndex) {
-if (gameState.gameBoard[tileIndex] === '' && gameState.gameCondition === 'incomplete') {
-    gameState.gameBoard[tileIndex] = gameState.playerTurn
-    checkCondition() //<--- Isn't complete
-    switchPlayer()
-    renderElements()
-}
+    if (gameState.gameBoard[tileIndex] === '' && gameState.gameCondition === 'incomplete') {
+        gameState.gameBoard[tileIndex] = gameState.playerTurn
+        checkCondition()
+        switchPlayer()
+        renderElements()
+    }
 }
 //----------------------------------------------------------------------------------------------------------------------------------
 // Switches the player symbol. 
 function switchPlayer() {
-if (gameState.playerTurn === 'X') {
-    gameState.playerTurn = 'O'
-} else {
-gameState.playerTurn = 'X'
-}
+    if (gameState.playerTurn === 'X') {
+        gameState.playerTurn = 'O'
+    } else {
+        gameState.playerTurn = 'X'
+    }
 }
 //----------------------------------------------------------------------------------------------------------------------------------
 // Checks whether game is a win, draw or incomplete (Congrats you suck)
-function checkCondition() {
-//const winningCombinations = [
-//    [0,1,2],[3,4,5],[6,7,8],
-//    [0,3,6],[1,4,7],[2,5,8],
-//    [0,4,9],[2,4,6]
-// ]
-// for(let i = 0; i < winningCombinations.length; i++) {
-//  const combinations = winningCombinations[i]
-// }
+function checkCondition() { //<--- Not Completed, Check Later On
+    const winningCombinations = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8],
+        [0, 3, 6], [1, 4, 7], [2, 5, 8],
+        [0, 4, 9], [2, 4, 6]
+    ]
+    for (let i = 0; i < winningCombinations.length; i++) {
+        const combinations = winningCombinations[i]
+    }
 }
 //----------------------------------------------------------------------------------------------------------------------------------
 initGame()
