97aa3fcbc0f8ee9959a75537f253c8270915a658
Brad Kerr <134312764+Bradpk@users.noreply.github.com>
2023-06-23 12:40:14 -0400

    g

--- js/main.js
+++ js/main.js
@@ -68,8 +68,6 @@ function clickAction(tileIndex) {
         checkCondition()
         renderElements()
         switchPlayer()
-
-
     }
 }
 //----------------------------------------------------------------------------------------------------------------------------------
@@ -87,7 +85,7 @@ function checkCondition() { //
     const winningCombinations = [
         [0, 1, 2], [3, 4, 5], [6, 7, 8],
         [0, 3, 6], [1, 4, 7], [2, 5, 8],
-        [0, 4, 9], [2, 4, 6]
+        [0, 4, 8], [2, 4, 6]
     ]
     for (let i = 0; i < winningCombinations.length; i++) {
         const [a, b, c] = winningCombinations[i]
@@ -96,8 +94,7 @@ function checkCondition() { //
             gameState.gameBoard[a] !== ''
         ) {
             gameState.gameCondition = 'win'
-
-
+            
         } else if (!gameState.gameBoard.includes('')) {
             gameState.gameCondition = 'draw'
         }
